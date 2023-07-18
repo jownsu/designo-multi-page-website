19d2ce8814da17a3f43bd05f9c381316595e1eef
Jhones <jhonesdigno666@gmail.com>
2023-07-18 22:49:24 +0800

    implemented navigation bar

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const menu_btn = document.querySelector("nav .menu");
+
+    /* Toggling of navigation on mobile */
+    menu_btn.addEventListener("click", () => {
+        document.querySelector("nav ul").classList.toggle("active");
+        menu_btn.classList.toggle("active");
+        document.querySelector(".overlay").classList.toggle("active");
+    });
+
+})
