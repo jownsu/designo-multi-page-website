1bea2ec3d9ffa36bc39a03f080f75ef0ba0e63fd
Jhones <jhonesdigno666@gmail.com>
2023-08-02 22:04:48 +0800

    Implemented contact page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const menu_btn = document.querySelector("nav .menu");
+    const contact_form = document.getElementById("contact_form");
+    const name_field = contact_form.querySelector("[name='name']");
+    const email_field = contact_form.querySelector("[name='email']");
+    const phone_field = contact_form.querySelector("[name='phone']");
+    const message_field = contact_form.querySelector("[name='message']");
 
     /* Toggling of navigation on mobile */
     menu_btn.addEventListener("click", () => {
@@ -9,4 +14,22 @@ document.addEventListener("DOMContentLoaded", () => {
         document.querySelector(".overlay").classList.toggle("active");
     });
 
+    /* Submitting of contact form */
+    contact_form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        validateField(name_field);
+        validateField(email_field);
+        validateField(phone_field);
+        validateField(message_field);
+    })
 })
+
+/* To validate the selected input element */
+const validateField = (element) => {
+    if(element.value.trim() === ""){
+        element.closest(".input_group").classList.add("error");
+    }
+    else{
+        element.closest(".input_group").classList.remove("error");
+    }
+}
